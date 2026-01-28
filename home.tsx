95812322bc014efdbfa56d07addef63c60fdb69b
Rendy <rendy.pramudya30@gmail.com>
2026-01-28 15:39:48 +0700

    update.tsx

---
+++ home.tsx
@@ -0,0 +1,15 @@
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Helllo, I'm Rendy Nayogi Pramudya</h1>
+      <p>Front-End Developer and UI/UX Designer Enthusiast</p>
+    </div>
+  );
+};
+
+const Helllo: React.FC = () => {
+  return <h2>Welcome to my portfolio website!</h2>;
+}
+
+
+export default Home;
